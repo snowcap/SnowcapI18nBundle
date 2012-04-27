66f5378025eda2bf11917a5c96400974f01370b2
Jerome Poskin <jerome@snowcap.be>
2012-04-27 15:01:57 +0200

    Getting locale features from CoreBundle

--- Twig/Extension/LocaleExtension.php
+++ Twig/Extension/LocaleExtension.php
@@ -11,9 +11,12 @@ class LocaleExtension extends \Twig_Extension
      */
     private $container;
 
-    public function __construct(ContainerInterface $container)
+    private $activeLocales = array();
+
+    public function __construct(ContainerInterface $container, array $activeLocales = array())
     {
         $this->container = $container;
+        $this->activeLocales = $activeLocales;
     }
 
     public function getGlobals()
@@ -23,8 +26,37 @@ class LocaleExtension extends \Twig_Extension
         );
     }
 
+    public function getFunctions()
+    {
+        return array('get_active_locales' => new \Twig_Function_Method($this, 'getActiveLocales'));
+    }
+
     public function getName()
     {
         return '_locale';
     }
+
+    public function setActiveLocales(array $locales)
+    {
+        $this->activeLocales = $locales;
+    }
+
+    public function getActiveLocales($locale = null)
+    {
+        if(empty($this->activeLocales)){
+            return array();
+        }
+        elseif($locale === null) {
+            $translatedLocales = $this->activeLocales;
+        }
+        else {
+            $displayLocales = Locale::getDisplayLocales($locale);
+            $translatedLocales = array_map(function($element) use($displayLocales){
+                return $displayLocales[$element];
+            }, $this->activeLocales);
+        }
+        return array_combine($this->activeLocales, $translatedLocales);
+    }
+
+
 }
